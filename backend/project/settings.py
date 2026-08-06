0705ed2186ab638322caccec26390214c14b86fe
sultana parvin kona <sultanakona259@gmail.com>
2026-08-06 20:18:09 +0600

    Add EMAIL_TIMEOUT

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -222,6 +222,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'sultanakona259@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
